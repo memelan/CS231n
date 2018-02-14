1d153a34fec59440e15a4ac2e21ba2f168ecd2d5
elainemeilan <memelan@users.noreply.github.com>
2018-02-14 18:31:28 +0800

    Update L3_Gradient_Descent.py

--- L3_Gradient_Descent.py
+++ L3_Gradient_Descent.py
@@ -3,3 +3,13 @@
 while True:
   weights_grad = evaluate_gradient(loss_fun, data, weights)
   weights += - step_size * weights_grad # perform parameter update
+
+
+# step size: learninig rate, super important, the first hyper-parameter needs to check
+
+
+# Vanilla Minibatch Gradient Descent
+while True:
+  data_batch = sample_training_data(data, 256) # sample 256 examples
+  weights_grad = evaluate_gradient(loss_fun, data_batch, weights)
+  weights += -step_size * weights_grad # perform parameter update
