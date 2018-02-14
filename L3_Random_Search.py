bc659c0648ec33723517fe8933f720f2d6cce566
elainemeilan <memelan@users.noreply.github.com>
2018-02-14 18:11:37 +0800

    Update L3_Random_Search.py

--- L3_Random_Search.py
+++ L3_Random_Search.py
@@ -14,3 +14,18 @@ for num in xrange(1000):
 # prints:
 # in attempt 0 the loss was 9.40, best 9.40
 # ...
+
+
+# see how well this works on test set
+
+# assume X_test is [3073 x 10000], Y_test [10000 x 1]
+scores = Wbest.dot(Xte_cols) # 10* 10000, the class scores for all the test examples
+# find the index with max score in each column (the predicted class)
+Yte_predict = np.argmax(scores, axis = 0)
+# and calculate accuracy (fraction of predictions that are correct)
+np. mean(Yte_predict == Yte)
+# returns 0.1555
+
+# SOTA state-of-the-arts ~0.95
+# might not use it in practice, but is a way you could potentially do
+# could work well in practice, if you got all the details right
