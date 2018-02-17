bb57590a9f079a9e7a4a2ca4cbb3706e3996ce3b
elainemeilan <memelan@users.noreply.github.com>
2018-02-17 15:26:41 +0800

    Update L4_Forward_backward_API.py

--- L4_Forward_backward_API.py
+++ L4_Forward_backward_API.py
@@ -1,1 +1,12 @@
 #
+class MultiplyGate(object):
+  def forward(x,y):
+    z = x*y
+    self.x = x # must keep these around!
+    self.y = y
+    return z
+  def backward(dz):
+    dx = self.y * dz # [dz/dx * dL/dz]
+    dy = self.x * dz # [dz/dy * dL/dz]
+    return [dx, dy]
+  
