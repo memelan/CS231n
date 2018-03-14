8802d778399452753dfd9626ee840b48ade59b79
elainemeilan <memelan@users.noreply.github.com>
2018-03-14 21:40:06 +0800

    Update L8_Conditional_tf_vs_pytorch.py

--- L8_Conditional_tf_vs_pytorch.py
+++ L8_Conditional_tf_vs_pytorch.py
@@ -1,1 +1,33 @@
 # 1 Pytorch
+N, D, H = 3, 4, 5
+
+x = Variable(torch.randn(N, D))
+w1 = Variable(torch.randn(D, H))
+w2 = Variable(torch.randn(D, H))
+
+z = 10
+if z > 0:
+    y = x.mm(w1)
+else:
+    y = x.mm(w2)
+ 
+
+# 2 TensorFlow
+N, D, H = 3, 4, 5
+x = tf.placeholder(tf.float32, shape=(N, D))
+z = tf.placeholder(tf.float32, shape=None)
+w1 = tf.placeholder(tf.float32, shape=(D, H))
+W2= tf.placeholder(tf.float32, shape=(D, H))
+
+def f1(): return tf.matmul(x, w1)
+def f2(): return tf.matmul(x, w2)
+y = tf.cond(tf.less(z, 0), f1, f2)
+
+with tf.Session() as sess:
+    values = {
+        x: np.random.randn(N, D),
+        z: 10,
+        w1: np.random.randn(D, H),
+        w2: np.random.randn(D, H)
+    }
+    y_val = sess.run(y, feed_dict_values)
