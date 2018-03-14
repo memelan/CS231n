63bdde8e22bc68e2061875c44fadb0e09a2e8634
elainemeilan <memelan@users.noreply.github.com>
2018-03-14 20:15:09 +0800

    Update L8_DL_Frameworks.py

--- L8_DL_Frameworks.py
+++ L8_DL_Frameworks.py
@@ -12,5 +12,15 @@ with tf.device ('/gpu:0): # one sentence to change the code running on CPU or GP
     a = x + y
     b = a + z
     c = tf.reduce_sum(b)
-                
-								
+        
+grad_x, grad_y, grad_z = tf.gradients(c, (x, y, z))
+
+with tf.Session() as sess:
+    values = {
+		    x: np.random.randn(N, D),
+			  y: np.random.randn(N, D),
+		  	z: np.random.randn(N, D),
+		}
+	  out = sess.run({c, grad_x, grad_y, grad_z},
+									feed_dict=values)
+		c_val, grad_x_val, grad_y_val, grad_z_val = out
